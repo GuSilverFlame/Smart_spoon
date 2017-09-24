e84edcec0da6d7b47cde88fe1a3a515d73e191a4
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-09-24 12:44:33 -0300

    added new libraries to deal with the i2c display

--- smart_ino.ino
+++ smart_ino.ino
@@ -1,3 +1,19 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
 
 void draw(int temp);
 
@@ -16,14 +32,18 @@ int heat_pin = 10;
 
 void setup(void) {
   Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
   analogReference(EXTERNAL);
   pinMode(heat_pin, OUTPUT);
 }
 
 void loop(void) 
 {
+  display.clearDisplay();
   int curr_temp = read_temp();
   draw(curr_temp);
+  display.display();
   if(curr_temp <= 50.0)
     digitalWrite(heat_pin, HIGH);
   else
@@ -33,11 +53,22 @@ void loop(void)
 
 void draw(int temp){
   char out_temp[6];
-  sprintf(out_temp, "%dºC\n", temp);
+  sprintf(out_temp, "%d C", temp);
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(25,20);
+  display.clearDisplay();
+  display.println(out_temp);
+  display.drawCircle(65, 20, 4, WHITE);
+  display.drawRect(20, 15, 80, 30, WHITE);
   Serial.print(out_temp);
   if(temp >= 50.0){
-    Serial.print("Pronto!");
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(8,1);
+    display.println("Pode se servir!");
   }
+  display.display();
 }
 float read_temp(){
   uint8_t i;
